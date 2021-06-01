1c1655046f98584b61e329044d4c9104f385d3a2
Fabio Frasson <fabio.frass@gmail.com>
2021-06-01 20:24:27 -0300

    Project upload

---
+++ src/main/java/com/fabiofrasson/prova01jun/Prova01junApplication.java
@@ -0,0 +1,13 @@
+package com.fabiofrasson.prova01jun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prova01junApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prova01junApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabiofrasson/prova01jun/Prova01junApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabiofrasson.prova01jun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova01junApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
